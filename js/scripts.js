49c64c9855eb1234daff5f151f07d3008096bfe4
Maxwel5 <maxwellwafulasim@gmail.com>
2019-07-22 08:05:28 +0300

    Providing effect to the portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,44 @@
 $(document).ready(function() {
-    $("#offer-p").hide();
-    $("#offer-p").click(function() {
-        $("#offer").hide().toggle();
-        $("#offer-p").slideUp();
+   $("#offer1").click(function() {
+        $("#offer-p1").show();
+        $("#offer1").hide();
     });
-    $("#offer").click(function() {
-        $("#offer-p").slideDown();
-        $("#offer").hide();
+    $("#offer-p1").click(function() {
+        $("#offer-p1").hide();
+        $("#offer1").show();
+    });
+    $("#offer2").click(function() {
+        $("#offer-p2").show();
+        $("#offer2").hide();
+    });
+    $("#offer-p2").click(function() {
+        $("#offer-p2").hide();
+        $("#offer2").show();
+    });
+    $("#offer3").click(function() {
+        $("#offer-p3").show();
+        $("#offer3").hide();
+    });
+    $("#offer-p3").click(function() {
+        $("#offer-p3").hide();
+        $("#offer3").show();
+    });
+});
+
+$(document).ready(function() {
+    $("#work4img").mouseenter(function() {
+        $("#work4").show();
+        $(this).addClass("fading");
+    }).mouseout(function () {
+        $("#work4").hide();
+        $(this).removeClass("fading");
+    });
+});
+
+$(document).ready(function() {
+    $("").mousehover(function() {
+        $("").show();
+    }).mouseout(function() {
+        $("").hide();
     });
 });
